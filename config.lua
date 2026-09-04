e8850d7a9d248ffbff0b0f0b060fda4e9067f3e0
SOBING GANTENG <alifhakim4413@gmail.com>
2026-09-04 14:21:13 +0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,3 @@
+Config = {}
+Config.ScreenshotWebhook = "https://discord.com/api/webhooks/1110361877843820544/9UjBKZboRqMP5yYVlTMTzrhEgLha5PyPgxX24LJCjpMcwieFfo5vNtl9tnHxGEN74i8w"
+Config.VideoWebhook = "https://discord.com/api/webhooks/1110361877843820544/9UjBKZboRqMP5yYVlTMTzrhEgLha5PyPgxX24LJCjpMcwieFfo5vNtl9tnHxGEN74i8w"
